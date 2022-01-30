575828d0c1e8269634f95a8f2f98d24856942f66
Sebastián Chavarry <69838707+sebastianperudev2001@users.noreply.github.com>
2022-01-30 13:20:25 -0500

    Html creado, falta modificar scripts de JS para generar tabla grid

--- scripts.js
+++ scripts.js
@@ -1,6 +1,25 @@
+const createGrid = () => 
+{
+    const contenedor = document.getElementById("grid");
+    for (let i = 1; i <= 10; i++) 
+    {
+        let fila = document.createElement("div");
+        fila.setAttribute("id","fila");
+        fila.setAttribute("class", "row");
+        for (let j = 1; j <= 10; j++) 
+        {
+            let cuadrado = document.createElement("div");
+            cuadrado.innerText= " ";
+            cuadrado.setAttribute("class", "col");
+            fila.appendChild(cuadrado)
+        } 
+        contenedor.appendChild(fila)
+    }
+}   
+
 const main = () => 
 {
-
+    createGrid(); 
 }
 
 window.onload = main; 
