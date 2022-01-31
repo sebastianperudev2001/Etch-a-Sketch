d4e781c9c13b9f30a3e182f7e0ea900ab3481c87
Sebastián Chavarry <69838707+sebastianperudev2001@users.noreply.github.com>
2022-01-31 11:53:06 -0500

    Ya colorea y se borra todo

--- scripts.js
+++ scripts.js
@@ -11,15 +11,44 @@ const createGrid = () =>
             let cuadrado = document.createElement("div");
             cuadrado.innerText= " ";
             cuadrado.setAttribute("class", "col");
+            cuadrado.setAttribute("id", `${i},${j}`)
             fila.appendChild(cuadrado)
-        } 
+        }  
         contenedor.appendChild(fila)
     }
 }   
 
+const hoverEffect = (event) => 
+{
+    const area = event.target; 
+    area.style.backgroundColor = "red";
+}
+
+const clearEffect = (event) => 
+{
+    for (let i = 1; i <= 10; i++) 
+    {
+        for (let j = 1; j <= 10; j++) 
+        {
+            document.getElementById(`${i},${j}`).style.backgroundColor = "white";
+        }
+    }
+}
+
 const main = () => 
 {
     createGrid(); 
+    for (let i = 1; i <= 10; i++) 
+    {
+        for (let j = 1; j <= 10; j++) 
+        {
+            document.getElementById(`${i},${j}`).addEventListener("mouseenter", hoverEffect);
+        }
+        
+    }
+
+    document.getElementById("but-clear").addEventListener("click", clearEffect);
+    
 }
 
 window.onload = main; 
