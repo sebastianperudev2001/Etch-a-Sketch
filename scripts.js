2129faf5a7321cdb36a4bbff0ca5811d0bd56daa
Sebastián Chavarry <69838707+sebastianperudev2001@users.noreply.github.com>
2022-02-03 19:38:38 -0500

    Botones de abajo funcionando al 100% (o eso quiero creer jeje)

--- scripts.js
+++ scripts.js
@@ -7,7 +7,6 @@ const updateFirst = (event) =>
     colorPincel = document.getElementById("color-input").value;
 }
 
-
 //Se aprovechan las clases predefinidas de bootstrap para poder crear columnas y filas en el div grid
 const createGrid = () => 
 {
@@ -36,10 +35,23 @@ const hoverEffect = (event) =>
     area.style.backgroundColor = colorPincel;
 }
 
+const colorButton = () => 
+{
+    for (let i = 1; i <= 10; i++) 
+    {
+        for (let j = 1; j <= 10; j++) 
+        {
+            document.getElementById(`${i},${j}`).removeEventListener("mouseenter", rainbowEffect);
+            document.getElementById(`${i},${j}`).removeEventListener("mouseenter", eraserEffect);
+            document.getElementById(`${i},${j}`).addEventListener("mouseenter", hoverEffect);
+        }   
+    }
+}
+
 
 //Permite borrar todo el grid mediante un inner loop a todos los cuadrillos
-const clearEffect = (event) => 
-{
+const clearEffect = () => 
+{    
     for (let i = 1; i <= 10; i++) 
     {
         for (let j = 1; j <= 10; j++) 
@@ -49,10 +61,69 @@ const clearEffect = (event) =>
     }
 }
 
+
+
+
+//https://stackoverflow.com/questions/1484506/random-color-generator
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+
+const rainbowEffect = (event) => 
+{
+    const area = event.target; 
+    area.style.backgroundColor = getRandomColor(); 
+}
+
+const rainbowButton = () => 
+{
+    
+    for (let i = 1; i <= 10; i++) 
+    {
+        for (let j = 1; j <= 10; j++) 
+        {
+            document.getElementById(`${i},${j}`).removeEventListener("mouseenter", hoverEffect);
+            document.getElementById(`${i},${j}`).removeEventListener("mouseenter", eraserEffect);
+            document.getElementById(`${i},${j}`).addEventListener("mouseenter",  rainbowEffect);
+        }
+        
+    }
+}
+
+const eraserEffect = (event) => 
+{
+    const area = event.target; 
+    area.style.backgroundColor = "white";
+}
+
+const eraserButton = () => 
+{
+    for (let i = 1; i <= 10; i++) 
+    {
+        for (let j = 1; j <= 10; j++) 
+        {
+            document.getElementById(`${i},${j}`).removeEventListener("mouseenter", rainbowEffect);
+            document.getElementById(`${i},${j}`).removeEventListener("mouseenter", hoverEffect);
+            document.getElementById(`${i},${j}`).addEventListener("mouseenter",  eraserEffect);
+
+        }   
+    }
+}
+
 const main = () => 
 {
     createGrid(); 
-
+    document.getElementById("but-clear").addEventListener("click", clearEffect);
+    document.getElementById("color-input").addEventListener("input", updateFirst, false); 
+    document.getElementById("but-rainbowMode").addEventListener("click", rainbowButton);
+    document.getElementById("but-colorMode").addEventListener("click", colorButton);
+    document.getElementById("but-eraser").addEventListener("click", eraserButton); 
     //A cada cuadrito se le otorga el evento hover 
     for (let i = 1; i <= 10; i++) 
     {
@@ -63,8 +134,7 @@ const main = () =>
         
     }
 
-    document.getElementById("but-clear").addEventListener("click", clearEffect);
-    document.getElementById("color-input").addEventListener("input", updateFirst, false); 
+   
     
 }
 
